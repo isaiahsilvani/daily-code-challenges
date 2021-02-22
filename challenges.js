b375d10a78d72a9cb51afffe1c4c6a555539b72b
isaiahsilvani <isaiahsilvani@outlook.com>
2021-02-22 15:55:58 -0500

    solved challenge 1

--- challenges.js
+++ challenges.js
@@ -25,9 +25,15 @@ Difficulty: Basic
 Prompt:
 
 Write a function called addOne that takes a single number as an argument and returns that number plus 1.
+-----*/
+function addOne(n) {
+  return n + 1;
+}
 
-Examples:
 
+
+/*------------
+Examples
 addOne(1) //=> 2
 addOne(-5) //=> -4
 -----------------------------------------------------------------------------*/
